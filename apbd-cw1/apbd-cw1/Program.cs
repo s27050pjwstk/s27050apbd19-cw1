7fe59878fbaadf3cbcc42be57f5846ae54a00666
Maciej Jankowski <s27050@pjwstk.edu.pl>
2025-03-05 15:16:18 +0100

    zmianaa

--- apbd-cw1/apbd-cw1/Program.cs
+++ apbd-cw1/apbd-cw1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, dziala czy nie!");
+Console.WriteLine("Hello, dziala czy niex!");
+
+
